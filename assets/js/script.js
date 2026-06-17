332c5069c85ae48b22cef57d3d99f8019eac9030
raigonlab <rrvnna@gmail.com>
2026-06-17 14:10:05 +0200

    Remove mouse edge-scroll speed boost from home gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -314,19 +314,6 @@ window.addEventListener('load', () => {
       });
     }
 
-    const deadZone = 0.2;
-    const absX     = Math.abs(mouseX);
-
-    if (absX > deadZone) {
-      const dir       = mouseX > 0 ? -1 : 1;
-      const intensity = (absX - deadZone) / (1 - deadZone);
-      const speed     = intensity * intensity * 1.2;
-
-      layers.forEach((_, i) => {
-        targetOffsets[i] += dir * speed * (1 - i * 0.15);
-      });
-    }
-
     layers.forEach((layer, i) => {
       const prev = targetOffsets[i];
       targetOffsets[i] = wrapOffset(targetOffsets[i], layer);
